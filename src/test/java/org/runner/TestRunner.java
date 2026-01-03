6467e13b15e1bd0c6b0adc521b9251ea7aed67f4
Deepak kumar <deepsmike1@gmail.com>
2026-01-03 15:01:45 +0530

    initial commit

---
+++ src/test/java/org/runner/TestRunner.java
@@ -0,0 +1,22 @@
+package org.runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"org.stepdefination"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json"},
+        
+        monochrome = true
+)
+public class TestRunner 
+{
+
+}
---
+++ src/test/java/org/stepdefination/LoginSteps.java
@@ -0,0 +1,83 @@
+package org.stepdefination;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginSteps 
+{
+	WebDriver driver;
+	
+	@Given("I am on the SauceDemo login page")
+	public void I_am_on_the_SauceDemo_login_page()
+	{
+		WebDriverManager.chromedriver().setup();
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+	}
+	
+	@When("I login with {string} and {string}")
+	public void i_login_with(String username, String password)
+	{
+		driver.findElement(By.id("user-name")).clear();
+		driver.findElement(By.id("user-name")).sendKeys(username);
+		
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys(password);
+		
+		driver.findElement(By.id("login-button")).click();
+	}
+	
+	@Then("the login result should be {string}")
+	public void the_login_result_should_be(String expected)
+	{
+		String currentURL = driver.getCurrentUrl();
+		
+		//Valid Login
+		
+		if (expected.equalsIgnoreCase("success"))
+		{
+			boolean isSuccess = currentURL.contains("inventory.html");
+			Assert.assertTrue("Expected successful login but URL was: " + currentURL, isSuccess);
+			
+		}
+		
+		
+		// Invalid Login
+		
+		else if (expected.equalsIgnoreCase("invalid"))
+		{
+			WebElement Err = driver.findElement(By.xpath("//h3[@data-test='error']"));
+			String msg = Err.getText().trim();
+			
+			String expectedMessage = "Epic sadface: Username and password do not match any user in this service";
+			
+			Assert.assertEquals("Invalid login message did not match!", expectedMessage, msg);
+				
+		}
+		
+		// LOCKED USER MESSAGE
+		
+		else if (expected.equalsIgnoreCase("locked"))
+		{
+			WebElement Err = driver.findElement(By.xpath("//h3[@data-test='error']"));
+			String msg = Err.getText().trim();
+			
+			String expectedMessage = "Epic sadface: Sorry, this user has been locked out.";
+			
+			Assert.assertEquals("Locked user message did not matach!", expectedMessage, msg);
+			
+		}
+		
+		driver.quit();
+	}
+}
